0de5b25082b6ed357000cd037af2af5bcd3c2017
Avi Cohen <cohena100@gmail.com>
2014-11-22 07:47:09 +0200

    Initial Commit

---
+++ TestBack/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestBack
+//
+//  Created by Avi Cohen on 11/22/14.
+//  Copyright (c) 2014 Avi Cohen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestBack/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestBack
+//
+//  Created by Avi Cohen on 11/22/14.
+//  Copyright (c) 2014 Avi Cohen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
